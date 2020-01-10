3299ebef08a212195d83250431576bfe1f560a86
ShadowFoxNixill <github@nix.nixill.net>
2020-01-10 10:53:14 -0500

    initial

---
+++ src/Main.cs
@@ -0,0 +1,12 @@
+namespace Nixill.GTFS {
+  public class FinalizerMain {
+    static void Main(string[] args) {
+      string path = null;
+
+      if (args.Length > 0) {
+        string test = args[0];
+        
+      }
+    }
+  }
+}
