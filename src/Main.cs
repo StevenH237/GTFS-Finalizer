8017ea7676b75a8db3dd28e56ced0e41fb114111
ShadowFoxNixill <github@nix.nixill.net>
2020-01-11 17:09:31 -0500

    this is getting slow

--- src/Main.cs
+++ src/Main.cs
@@ -1,12 +1,21 @@
+using System.IO;
+using System;
+
 namespace Nixill.GTFS {
   public class FinalizerMain {
     static void Main(string[] args) {
       string path = null;
 
-      if (args.Length > 0) {
-        string test = args[0];
-        
+      if (args.Length > 0) path = args[0];
+      else {
+        Console.Write("Select a folder: ");
+        path = Console.ReadLine();
       }
+
+      Directory.SetCurrentDirectory(path);
+
+      // Make sure we have directories
+      
     }
   }
 }
